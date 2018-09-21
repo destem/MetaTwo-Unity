a70d67a6b42c04102deacdcff2f5d97fbf7f8d29
Marc <marc.destefano@gmail.com>
2018-09-21 16:57:19 -0400

    Attempting stream Peek() method from https://docs.microsoft.com/en-us/dotnet/api/system.io.streamreader.peek

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -65,8 +65,11 @@ public class StartGame : MonoBehaviour
         {
             //print(reader.ReadToEnd());
             //print("---------");
-            comp.log.eyetrackString = reader.ReadLine();
-            comp.log.LogEyeTracker();
+            //while (reader.Peek() > -1)
+            //{
+                comp.log.eyetrackString = reader.ReadLine();
+                comp.log.LogEyeTracker();
+            //}
 
             // EX: Server says: <REC TIME="38.41262" FPOGX="0.41352" FPOGY="0.80682" FPOGS="38.36427" FPOGD="0.04835" FPOGID="93" FPOGV="1" BPOGX="0.43701" BPOGY="0.81848" BPOGV="1" />
             //print("Server says: " + data);
