8d08ac2360ada472862f4281e535d403c5292eee
Marc Destefano <marc.destefano@gmail.com>
2018-09-24 14:51:55 -0400

    Trying ways of reading multiple lines from reader stream, no success yet

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -65,10 +65,15 @@ public class StartGame : MonoBehaviour
         {
             //print(reader.ReadToEnd());
             //print("---------");
+            
             //while (reader.Peek() > -1)
             //{
                 comp.log.eyetrackString = reader.ReadLine();
                 comp.log.LogEyeTracker();
+            //}
+            //while ((comp.log.eyetrackString = reader.ReadLine()) != null)
+            //{
+            //    comp.log.LogEyeTracker();
             //}
 
             // EX: Server says: <REC TIME="38.41262" FPOGX="0.41352" FPOGY="0.80682" FPOGS="38.36427" FPOGD="0.04835" FPOGID="93" FPOGV="1" BPOGX="0.43701" BPOGY="0.81848" BPOGV="1" />
